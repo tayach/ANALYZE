c28e02271c08f83f3683136ce5d2db94a23637f5
Stephan Balduin <stephan.balduin@offis.de>
2022-09-08 10:15:10 +0200

    Initial commit with ci

---
+++ gitlab-ci/build_midas_baseimage.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ash
+
+REQUIREMENTS_IN="../requirements.txt"
+
+echo "wheel" > requirements.txt
+# get midas line from requirements.txt but exclude lines beginning with a hashtag (-> comments)
+echo $(cat ${REQUIREMENTS_IN} | grep midas-mosaik | grep -E "^[^#]") >> requirements.txt
+
+REPOSITORY="registry.gitlab.com/pyrate-project/smasher/midas-mosaik-baseimage"
+MIDAS_MOSAIK_VERSION=$(cat ${REQUIREMENTS_IN} | grep midas-mosaik | sed -r 's/midas-mosaik *~*>*<*=* *//')
+echo "midas-mosaik version: ${MIDAS_MOSAIK_VERSION}"
+echo ${MIDAS_MOSAIK_VERSION}>midas_version.txt
+
+docker manifest inspect ${REPOSITORY}:${MIDAS_MOSAIK_VERSION} > /dev/null
+EXIT_CODE=$?
+# if EXIT_CODE == 1, this docker image tag doesn't exist, yet -> build and push it
+if [ $EXIT_CODE -eq 1 ]; then
+    docker build -t ${REPOSITORY}:${MIDAS_MOSAIK_VERSION} . -f Dockerfile.midas_base
+    docker push ${REPOSITORY}:${MIDAS_MOSAIK_VERSION}
+fi
+
