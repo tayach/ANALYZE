bbbd6bcfddc261e035d6a714e9568f84918885ea
Stephan Balduin <stephan.balduin@offis.de>
2022-09-08 11:22:18 +0200

    Added qmarket scenario

--- src/smasher/scripts/qmarket_scenario.py
+++ src/smasher/scripts/qmarket_scenario.py
@@ -5,11 +5,11 @@ import midas.api as mia
 def main():
     scenario_path = os.path.abspath(
         os.path.join(
-            __file__, "..", "..", "scenarios", "powergrid_attack_mv.yml"
+            __file__, "..", "..", "scenarios", "qmarket_mv.yml"
         )
     )
 
-    mia.run("attack_mv_dynamic", {"end": 60*60}, scenario_path)
+    mia.run("qmarket_mv", {}, scenario_path)
 
 
 if __name__ == "__main__":
